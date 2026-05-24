34f5dec764841c9c45b365d3a6c9a8ed97432d27
Nandini Bhimineni <yoganandini9@gmail.com>
2026-05-25 01:36:15 +0530

    Completed product image storage module

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -1,18 +1,12 @@
 const cloudinary = require('cloudinary').v2;
 
 cloudinary.config({
-<<<<<<< HEAD
 
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
 
     api_key: process.env.CLOUDINARY_API_KEY,
 
     api_secret: process.env.CLOUDINARY_API_SECRET
-=======
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
->>>>>>> 4df0be7 (Completed User Image Storage Module)
 });
 
 module.exports = cloudinary;

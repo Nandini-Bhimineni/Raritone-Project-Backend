f4f4ba71d0761bdb5f6cda5134cac9f23f969945
Nandini Bhimineni <yoganandini9@gmail.com>
2026-05-24 23:08:38 +0530

    Created centralized image model

--- models/Image.js
+++ models/Image.js
@@ -1,13 +1,39 @@
 const mongoose = require("mongoose");
 
 const imageSchema = new mongoose.Schema({
-  imageUrl: String,
-  type: String,
-  uploadedBy: String,
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "user",
+  },
+
+  imageUrl: {
+    type: String,
+    required: true,
+  },
+
+  imageType: {
+    type: String,
+
+    enum: [
+      "profile",
+      "avatar",
+      "product",
+      "tryon",
+      "fashion",
+    ],
+  },
+
+  publicId: {
+    type: String,
+  },
+
   uploadedAt: {
     type: Date,
     default: Date.now,
   },
 });
 
-module.exports = mongoose.model("Image", imageSchema);
+module.exports = mongoose.model(
+  "Image",
+  imageSchema
+);
